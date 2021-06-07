398635bab3feb30c02407b7d97440a5dc0e15fbc
Daniel Lafontant <ldanieljajr@gmail.com>
2021-06-07 10:25:41 -0400

    index: added Schema

--- models/index.js
+++ models/index.js
@@ -1,1 +1,28 @@
+require('dotenv').config();
+const mongoose = require('mongoose')
 
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+const db = mongoose.connection;
+
+
+db.once('open', () => {
+    console.log(`Connect to mongoDDB at ${db.host}: ${db.port}`)
+}); 
+
+
+db.on('error', (error)=> {
+    console.log(`Database error`, errror);
+})
+
+
+//import alll of your models
+const User = require('./User');
+
+// export all the models fro this file
+module.exports = {
+    User, 
+}
